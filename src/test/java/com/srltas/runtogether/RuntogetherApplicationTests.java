0037065544c8d76f0fe974f836ae493f76459f51
Srltas <kdm97912@gmail.com>
2024-09-04 00:35:16 +0900

    chore: SpringBoot, Gradle 설정 파일 추가

---
+++ src/test/java/com/srltas/runtogether/RuntogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srltas.runtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
