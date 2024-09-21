770021244d43de72194d6199acd3bfc1dcf795cc
Srltas <kdm97912@gmail.com>
2024-09-22 01:24:06 +0900

    test: 동네 인증 테스트 코드 리펙토링
    - Hamcrest를 사용해 테스트 코드 수정
    - BDD 스타일 적용

--- src/test/java/com/srltas/runtogether/domain/model/neighborhood/NeighborhoodTest.java
+++ src/test/java/com/srltas/runtogether/domain/model/neighborhood/NeighborhoodTest.java
@@ -1,16 +1,20 @@
 package com.srltas.runtogether.domain.model.neighborhood;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.srltas.runtogether.domain.model.location.DistanceCalculator;
 import com.srltas.runtogether.domain.model.location.Location;
 
+@ExtendWith(MockitoExtension.class)
 class NeighborhoodTest {
 
 	@Mock
@@ -22,30 +26,30 @@ class NeighborhoodTest {
 
 	@BeforeEach
 	public void setUp() {
-		openMocks(this);
-
 		currentLocation = new Location(37.505858, 127.058319);
 		neighborhoodLocation = new Location(37.517347, 127.047382);
 		neighborhood = new Neighborhood("Gangnam", neighborhoodLocation, 7.0, distanceCalculator);
 	}
 
 	@Test
+	@DisplayName("사용자와 동네 기준이 설정된 범위보다 작을 때, 동네 내에 있다고 판단")
 	public void testIsWithinBoundary_WhenUserInSideBoundary() {
-		when(distanceCalculator.calculateDistanceBetween(currentLocation, neighborhoodLocation)).thenReturn(5.0);
+		given(distanceCalculator.calculateDistanceBetween(currentLocation, neighborhoodLocation)).willReturn(5.0);
 
 		boolean isInSide = neighborhood.isWithinBoundary(currentLocation);
 
-		assertTrue(isInSide);
-		verify(distanceCalculator).calculateDistanceBetween(currentLocation, neighborhoodLocation);
+		assertThat(isInSide, is(true));
+		then(distanceCalculator).should().calculateDistanceBetween(currentLocation, neighborhoodLocation);
 	}
 
 	@Test
+	@DisplayName("사용자와 동네 기준이 설정된 범위보다 클 때, 동네 밖에 있다고 판단")
 	public void testIsWithinBoundary_WhenUserOutsideBoundary() {
 		when(distanceCalculator.calculateDistanceBetween(currentLocation, neighborhoodLocation)).thenReturn(10.0);
 
 		boolean isInSide = neighborhood.isWithinBoundary(currentLocation);
 
-		assertFalse(isInSide);
-		verify(distanceCalculator).calculateDistanceBetween(currentLocation, neighborhoodLocation);
+		assertThat(isInSide, is(false));
+		then(distanceCalculator).should().calculateDistanceBetween(currentLocation, neighborhoodLocation);
 	}
 }
