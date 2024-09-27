1864558924cbdcc53a89d324ec0a10f08ae7f72d
Srltas <kdm97912@gmail.com>
2024-09-28 02:58:58 +0900

    refactor: 유틸리티 클래스에 private 생성자 추가

--- src/main/java/com/srltas/runtogether/application/mappper/LocationNeighborhoodVerifyRequestMapper.java
+++ src/main/java/com/srltas/runtogether/application/mappper/LocationNeighborhoodVerifyRequestMapper.java
@@ -5,6 +5,13 @@ import com.srltas.runtogether.domain.model.location.Location;
 
 public class LocationNeighborhoodVerifyRequestMapper {
 
+	/**
+	 * 유틸리티 클래스의 인스턴스화를 방지하기 위해 private 생성자를 선언합니다.
+	 */
+	private LocationNeighborhoodVerifyRequestMapper() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
+
 	public static Location toDomain(LocationNeighborhoodVerifyRequest locationNeighborhoodVerifyRequest) {
 		return new Location(locationNeighborhoodVerifyRequest.latitude(), locationNeighborhoodVerifyRequest.longitude());
 	}
--- src/main/java/com/srltas/runtogether/application/mappper/UserNeighborhoodVerifyRequestMapper.java
+++ src/main/java/com/srltas/runtogether/application/mappper/UserNeighborhoodVerifyRequestMapper.java
@@ -5,6 +5,13 @@ import com.srltas.runtogether.domain.model.user.User;
 
 public class UserNeighborhoodVerifyRequestMapper {
 
+	/**
+	 * 유틸리티 클래스의 인스턴스화를 방지하기 위해 private 생성자를 선언합니다.
+	 */
+	private UserNeighborhoodVerifyRequestMapper() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
+
 	public static User toDomain(UserNeighborhoodVerifyRequest userNeighborhoodVerifyRequest) {
 		return new User(userNeighborhoodVerifyRequest.id(), userNeighborhoodVerifyRequest.name());
 	}
