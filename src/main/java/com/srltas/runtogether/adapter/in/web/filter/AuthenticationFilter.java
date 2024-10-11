deb261f475b7156f346760c9b8be0ad0f52ad64d
Srltas <kdm97912@gmail.com>
2024-10-11 13:45:51 +0900

    refactor: OncePerRequestFilter를 상속하도록 수정
     - AuthenticationFilter는 해당 요청이 로그인된 사용자의 요청인지 확인하는 필터입니다
     - 해당 필터는 요청 당 한 번만 실행되면 되므로 한 번만 실행될 수 있도록 AuthenticationFileter를 상속했습니다.

--- src/main/java/com/srltas/runtogether/adapter/in/web/filter/AuthenticationFilter.java
+++ src/main/java/com/srltas/runtogether/adapter/in/web/filter/AuthenticationFilter.java
@@ -5,31 +5,27 @@ import static com.srltas.runtogether.common.SessionAttribute.*;
 
 import java.io.IOException;
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import com.srltas.runtogether.adapter.out.session.SessionStorage;
 import com.srltas.runtogether.adapter.out.session.UserSessionDTO;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class AuthenticationFilter implements Filter {
+public class AuthenticationFilter extends OncePerRequestFilter {
 
 	private final SessionStorage sessionStorage;
 
 	@Override
-	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
 		IOException,
 		ServletException {
-		HttpServletRequest request = (HttpServletRequest)servletRequest;
-		HttpServletResponse response = (HttpServletResponse)servletResponse;
-
 		String authorizationHeader = request.getHeader(AUTHORIZATION);
 		String token = extractToken(authorizationHeader);
 
