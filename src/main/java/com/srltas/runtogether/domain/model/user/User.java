0f8f930991e94ddcd33f147f041393ffd4509bc0
Srltas <kdm97912@gmail.com>
2024-10-18 21:36:25 +0900

    refactor: 사용자 동네 자료구조 변경
    - 사용자 동네를 저장할 때 순서가 중요하지 않다
    - 다른 자료구조보다 자료의 조회 속도가 빠른 HashMap을 사용하기로 결정

--- src/main/java/com/srltas/runtogether/domain/model/user/User.java
+++ src/main/java/com/srltas/runtogether/domain/model/user/User.java
@@ -1,8 +1,9 @@
 package com.srltas.runtogether.domain.model.user;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import static java.util.Objects.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import com.srltas.runtogether.domain.exception.CommonException;
 import com.srltas.runtogether.domain.model.neighborhood.Neighborhood;
@@ -16,29 +17,27 @@ public class User {
 
 	private final Long id;
 	private final String name;
-	private final List<UserNeighborhood> userNeighborhoods = new ArrayList<>();
+	private final Map<Integer, UserNeighborhood> userNeighborhoods = new HashMap<>();
 
 	public void addNeighborhood(Neighborhood neighborhood) {
 		if (userNeighborhoods.size() >= 2) {
 			throw new CommonException("최대 2개의 동네만 등록할 수 있습니다.");
 		}
 
-		if (userNeighborhoods.stream()
-			.anyMatch(un -> un.getNeighborhood().getId() == neighborhood.getId())) {
+		if (userNeighborhoods.containsKey(neighborhood.getId())) {
 			throw new CommonException("이미 등록된 동네입니다.");
 		}
-		userNeighborhoods.add(new UserNeighborhood(neighborhood));
+
+		userNeighborhoods.put(neighborhood.getId(), new UserNeighborhood(neighborhood));
 	}
 
 	public void verifiedNeighborhood(int neighborhoodId) {
-		Optional<UserNeighborhood> userNeighborhood = userNeighborhoods.stream()
-			.filter(un -> un.getNeighborhood().getId() == neighborhoodId)
-			.findFirst();
+		UserNeighborhood userNeighborhood = userNeighborhoods.get(neighborhoodId);
 
-		if (userNeighborhood.isPresent()) {
-			userNeighborhood.get().verifyNeighborhood();
-		} else {
+		if (isNull(userNeighborhood)) {
 			throw new CommonException("해당 동네가 등록되지 않았습니다.");
 		}
+
+		userNeighborhood.verifyNeighborhood();
 	}
 }
