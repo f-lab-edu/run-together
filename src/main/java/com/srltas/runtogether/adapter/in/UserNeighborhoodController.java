42dac617e2738d82312ad49c3c493a82027d4596
Srltas <kdm97912@gmail.com>
2024-10-27 15:58:36 +0900

    feat: 내 동네 등록 Controller 구현

---
+++ src/main/java/com/srltas/runtogether/adapter/in/UserNeighborhoodController.java
@@ -0,0 +1,44 @@
+package com.srltas.runtogether.adapter.in;
+
+import static com.srltas.runtogether.adapter.in.web.common.SessionAttribute.*;
+import static com.srltas.runtogether.adapter.in.web.common.UrlConstants.*;
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.srltas.runtogether.adapter.in.web.dto.AddUserNeighborhoodRequest;
+import com.srltas.runtogether.adapter.out.session.UserSessionDTO;
+import com.srltas.runtogether.application.port.in.AddUserNeighborhood;
+import com.srltas.runtogether.application.port.in.AddUserNeighborhoodCommand;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "내 동네 API", description = "내 동네 API")
+@RestController
+@RequiredArgsConstructor
+public class UserNeighborhoodController {
+
+	private final AddUserNeighborhood addUserNeighborhood;
+
+	@Operation(
+		summary = "내 동네 등록",
+		description = "사용자가 특정 동네를 자신의 동네로 등록합니다."
+	)
+	@ApiResponse(responseCode = "200", description = "내 동네 등록 성공")
+	@PostMapping(USER_NEIGHBORHOOD_REGISTRATION)
+	public ResponseEntity<Void> addUserNeighborhood(
+		@RequestBody @Valid AddUserNeighborhoodRequest request, HttpSession session) {
+		UserSessionDTO userSession = (UserSessionDTO)session.getAttribute(USER_SESSION);
+		addUserNeighborhood.addNeighborhood(
+			new AddUserNeighborhoodCommand(userSession.userId(), request.neighborhoodId()));
+		return new ResponseEntity<>(CREATED);
+	}
+}
--- src/main/java/com/srltas/runtogether/adapter/in/web/common/UrlConstants.java
+++ src/main/java/com/srltas/runtogether/adapter/in/web/common/UrlConstants.java
@@ -5,4 +5,5 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class UrlConstants {
 	public final String NEIGHBORHOOD_VERIFICATION = "/neighborhood/verification";
+	public final String USER_NEIGHBORHOOD_REGISTRATION = "/user-neighborhood";
 }
