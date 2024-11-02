1df8c1d8596d9322e7eef40b193923bc1c8e0493
Srltas <kdm97912@gmail.com>
2024-11-02 10:36:33 +0900

    feat: 공통 예외 처리 구현
    - Http 상태 코드를 공통 예외 처리 핸들러에서 결정해서 넣도록 구현

---
+++ src/main/java/com/srltas/runtogether/common/exception/handler/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.srltas.runtogether.common.exception.handler;
+
+import com.srltas.runtogether.common.exception.code.ErrorCode;
+
+public record ErrorResponse(
+	int code,
+	String msg
+) {
+	public static ErrorResponse of(int code, String msg) {
+		return new ErrorResponse(code, msg);
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode) {
+		return new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+	}
+}
---
+++ src/main/java/com/srltas/runtogether/common/exception/handler/RunTogetherExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.srltas.runtogether.common.exception.handler;
+
+import static com.srltas.runtogether.common.exception.code.CommonErrorCode.*;
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import com.srltas.runtogether.common.exception.EntityNotFoundException;
+import com.srltas.runtogether.common.exception.UnauthorizedException;
+
+@RestControllerAdvice
+public class RunTogetherExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleEntityNotFoundException(final EntityNotFoundException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(ex.getErrorCode()), NOT_FOUND);
+	}
+
+	@ExceptionHandler(UnauthorizedException.class)
+	public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(UNAUTHORIZED_REQUEST), FORBIDDEN);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNoResourceFoundException(final NoResourceFoundException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(NO_RESOURCE_FOUND), NOT_FOUND);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(INVALID_REQUEST_PARAMETER), BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HttpMediaTypeException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final HttpMediaTypeException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(INVALID_HEADER_REQUEST), BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MissingRequestValueException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MissingRequestValueException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(INVALID_HEADER_REQUEST), BAD_REQUEST);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleInternalServerError(final Exception ex) {
+		return new ResponseEntity<>(ErrorResponse.of(SERVER_ERROR), INTERNAL_SERVER_ERROR);
+	}
+}
