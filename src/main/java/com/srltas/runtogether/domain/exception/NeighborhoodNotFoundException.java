521c0807ba54ece68e995439b0a34fd4640318d0
Srltas <kdm97912@gmail.com>
2024-11-02 10:50:43 +0900

    refactor: 기존에 사용하던 Exception 제거

--- src/main/java/com/srltas/runtogether/domain/exception/NeighborhoodNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.srltas.runtogether.domain.exception;
-
-public class NeighborhoodNotFoundException extends RuntimeException {
-
-	public NeighborhoodNotFoundException() {
-		super("Neighborhood not found");
-	}
-}
