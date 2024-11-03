db6bb44fe8196eb48308f1b1529c4efe2d0f87d1
Srltas <kdm97912@gmail.com>
2024-11-04 02:03:48 +0900

    refactor: null, 공백, 길이, 형식 검증 추가

--- src/main/java/com/srltas/runtogether/adapter/in/web/dto/AddUserNeighborhoodRequest.java
+++ src/main/java/com/srltas/runtogether/adapter/in/web/dto/AddUserNeighborhoodRequest.java
@@ -1,9 +1,11 @@
 package com.srltas.runtogether.adapter.in.web.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record AddUserNeighborhoodRequest(
-	@NotNull
+	@NotBlank
+	@Pattern(regexp = "^usr_([a-fA-F0-9]{8}(-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12})$")
 	String neighborhoodId
 ) {
 }
--- src/main/java/com/srltas/runtogether/adapter/in/web/dto/NeighborhoodVerificationRequest.java
+++ src/main/java/com/srltas/runtogether/adapter/in/web/dto/NeighborhoodVerificationRequest.java
@@ -2,7 +2,9 @@ package com.srltas.runtogether.adapter.in.web.dto;
 
 import org.hibernate.validator.constraints.Range;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record NeighborhoodVerificationRequest(
 
@@ -14,7 +16,8 @@ public record NeighborhoodVerificationRequest(
 	@Range(min = -180, max = 180, message="경도는 -180에서 180 사이여야 합니다.")
 	double longitude,
 
-	@NotNull
+	@NotBlank
+	@Pattern(regexp = "^nhb_([a-fA-F0-9]{8}(-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12})$")
 	String neighborhoodId
 ) {
 }
