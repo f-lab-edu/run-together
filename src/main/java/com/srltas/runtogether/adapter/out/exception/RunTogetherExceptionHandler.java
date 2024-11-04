6126966dca8d249dfa543ae42f729389c5b728a3
Srltas <kdm97912@gmail.com>
2024-11-04 17:26:09 +0900

    refactor: 스프링 제공 예외 처리 추가
    - HttpMessageNotReadableException: 요청 본문이 읽을 수 없는 경우
    - HttpRequestMethodNotSupportedException: 지원되지 않는 HTTP 메서드가 호출될 경우
    - HttpMediatypeNotSupportedException: 클라이언트가 서버에세 지원하지 않는 미디어 타입을 요청하는 경우
    - MissingServletRequestParameterException: 필수 요청 파라미터가 누락된 경우
    - TypeMismatchException: 타입 불일치가 발생하는 경우
    - BindException: 요청 파라미터 바인딩에 실패하는 경우
    - ConstraintViolationException: 제약 조건을 위반하는 경우

--- src/main/java/com/srltas/runtogether/adapter/out/exception/RunTogetherExceptionHandler.java
+++ src/main/java/com/srltas/runtogether/adapter/out/exception/RunTogetherExceptionHandler.java
@@ -1,20 +1,32 @@
 package com.srltas.runtogether.adapter.out.exception;
 
+import static com.srltas.runtogether.common.exception.code.CommonErrorCode.METHOD_NOT_ALLOWED;
+import static com.srltas.runtogether.common.exception.code.CommonErrorCode.UNSUPPORTED_MEDIA_TYPE;
 import static com.srltas.runtogether.common.exception.code.CommonErrorCode.*;
 import static org.springframework.http.HttpStatus.*;
 
+import java.net.BindException;
+
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import com.srltas.runtogether.common.exception.ErrorResponse;
 import com.srltas.runtogether.common.exception.BusinessException;
+import com.srltas.runtogether.common.exception.ErrorResponse;
 import com.srltas.runtogether.common.exception.UnauthorizedException;
+
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class RunTogetherExceptionHandler {
@@ -30,8 +42,52 @@ public class RunTogetherExceptionHandler {
 	}
 
 	@ExceptionHandler(UnauthorizedException.class)
-	public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex) {
+	public ResponseEntity<ErrorResponse> handleUnauthorizedException(final UnauthorizedException ex) {
 		return new ResponseEntity<>(ErrorResponse.of(UNAUTHORIZED_REQUEST), FORBIDDEN);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
+		final HttpMessageNotReadableException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(INVALID_REQUEST_BODY), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+		final HttpRequestMethodNotSupportedException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(METHOD_NOT_ALLOWED), HttpStatus.METHOD_NOT_ALLOWED);
+	}
+
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(
+		final HttpMediaTypeNotSupportedException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(UNSUPPORTED_MEDIA_TYPE), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+	}
+
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
+		final MissingServletRequestParameterException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(MISSING_PARAMETER), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(TypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleTypeMismatchException(final TypeMismatchException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(TYPE_MISMATCH), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(BindException.class)
+	public ResponseEntity<ErrorResponse> handleBindException(final BindException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(BINDING_ERROR), BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorResponse> handleConstraintViolationException(final ConstraintViolationException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(CONSTRAINT_VIOLATION), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(final NoHandlerFoundException ex) {
+		return new ResponseEntity<>(ErrorResponse.of(NO_HANDLER_FOUND), HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(NoResourceFoundException.class)
