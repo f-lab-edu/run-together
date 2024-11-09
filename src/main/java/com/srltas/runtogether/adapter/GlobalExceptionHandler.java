51ac596f148b3f35dd02bfb4241bf535e117cb96
Srltas <kdm97912@gmail.com>
2024-11-10 01:45:14 +0900

    refactor: GlobalExceptionHandler error log 추가

--- src/main/java/com/srltas/runtogether/adapter/GlobalExceptionHandler.java
+++ src/main/java/com/srltas/runtogether/adapter/GlobalExceptionHandler.java
@@ -24,81 +24,102 @@ import com.srltas.runtogether.adapter.in.exception.UnauthorizedException;
 import com.srltas.runtogether.application.exception.EntityNotFoundException;
 import com.srltas.runtogether.common.exception.ErrorResponse;
 import com.srltas.runtogether.common.exception.RunTogetherException;
+import com.srltas.runtogether.common.exception.code.ErrorCode;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EntityNotFoundException.class)
 	protected ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+		logError(ex.getErrorCode());
 		return new ResponseEntity<>(ErrorResponse.of(ex.getErrorCode()), HttpStatus.NOT_FOUND);
 	}
 
 	@ExceptionHandler(UnauthorizedException.class)
 	protected ResponseEntity<Object> handleUnauthorizedException(UnauthorizedException ex) {
+		logError(ex.getErrorCode());
 		return new ResponseEntity<>(ErrorResponse.of(ex.getErrorCode()), HttpStatus.FORBIDDEN);
 	}
 
 	@ExceptionHandler(RunTogetherException.class)
 	protected ResponseEntity<Object> handleRunTogetherException(RunTogetherException ex) {
+		logError(ex.getErrorCode());
 		return new ResponseEntity<>(ErrorResponse.of(ex.getErrorCode()), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
 		HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(METHOD_NOT_ALLOWED);
 		return new ResponseEntity<>(ErrorResponse.of(METHOD_NOT_ALLOWED), HttpStatus.METHOD_NOT_ALLOWED);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
 		HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(UNSUPPORTED_MEDIA_TYPE);
 		return new ResponseEntity<>(ErrorResponse.of(UNSUPPORTED_MEDIA_TYPE), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
 		HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(NOT_ACCEPTABLE);
 		return new ResponseEntity<>(ErrorResponse.of(NOT_ACCEPTABLE), HttpStatus.NOT_ACCEPTABLE);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMissingPathVariable(
 		MissingPathVariableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(MISSING_PATH_VARIABLE);
 		return new ResponseEntity<>(ErrorResponse.of(MISSING_PATH_VARIABLE), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMissingServletRequestParameter(
 		MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(MISSING_PARAMETER);
 		return new ResponseEntity<>(ErrorResponse.of(MISSING_PARAMETER), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleServletRequestBindingException(
 		ServletRequestBindingException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(BINDING_ERROR);
 		return new ResponseEntity<>(ErrorResponse.of(BINDING_ERROR), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(INVALID_REQUEST_BODY);
 		return new ResponseEntity<>(ErrorResponse.of(INVALID_REQUEST_BODY), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleTypeMismatch(
 		TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(TYPE_MISMATCH);
 		return new ResponseEntity<>(ErrorResponse.of(TYPE_MISMATCH), HttpStatus.BAD_REQUEST);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(
 		HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		logError(INVALID_REQUEST_BODY);
 		return new ResponseEntity<>(ErrorResponse.of(INVALID_REQUEST_BODY), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleInternalServerException(Exception ex) {
+		logError(SERVER_ERROR);
 		return new ResponseEntity<>(ErrorResponse.of(SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	private void logError(ErrorCode errorCode) {
+		log.error("errorCode={}: message={}", errorCode.getCode(), errorCode.getMessage());
 	}
 }
