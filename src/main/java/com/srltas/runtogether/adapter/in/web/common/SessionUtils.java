d828d961cb3235d8c674c71e0744d7d76de064cc
Srltas <kdm97912@gmail.com>
2024-11-10 00:49:24 +0900

    feat: client 요청에 대한 로그를 남기는 Filter 추가

--- src/main/java/com/srltas/runtogether/adapter/in/web/common/SessionUtils.java
+++ src/main/java/com/srltas/runtogether/adapter/in/web/common/SessionUtils.java
@@ -1,6 +1,9 @@
 package com.srltas.runtogether.adapter.in.web.common;
 
+import static com.srltas.runtogether.adapter.in.web.common.AuthConstants.*;
 import static com.srltas.runtogether.adapter.in.web.common.SessionAttribute.*;
+
+import java.util.Optional;
 
 import com.srltas.runtogether.adapter.out.session.UserSessionDTO;
 
@@ -12,5 +15,12 @@ public class SessionUtils {
 
 	public UserSessionDTO getUserSessionDTO(HttpSession session) {
 		return (UserSessionDTO)session.getAttribute(USER_SESSION);
+	}
+
+	public Optional<String> extractUserIdToken(String authorizationHeader) {
+		if (authorizationHeader != null && authorizationHeader.startsWith(BEARER_TOKEN_PREFIX)) {
+			return Optional.of(authorizationHeader.substring(BEARER_TOKEN_LENGTH));
+		}
+		return Optional.empty();
 	}
 }
---
+++ src/main/java/com/srltas/runtogether/adapter/in/web/filter/RequestLogFilter.java
@@ -0,0 +1,40 @@
+package com.srltas.runtogether.adapter.in.web.filter;
+
+import static com.srltas.runtogether.adapter.in.web.common.AuthConstants.*;
+import static com.srltas.runtogether.adapter.in.web.common.SessionUtils.*;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RequestLogFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		MDC.put("requestId", UUID.randomUUID().toString());
+		MDC.put("userIdToken", extractUserIdToken(request.getHeader(AUTHORIZATION)).orElse("unauthenticated"));
+		MDC.put("method", request.getMethod());
+		MDC.put("path", request.getRequestURI());
+		MDC.put("clientIp", request.getRemoteAddr());
+
+		long startTime = System.currentTimeMillis();
+		try {
+			filterChain.doFilter(request, response);
+		} finally {
+			MDC.put("durationMs", String.valueOf(System.currentTimeMillis() - startTime));
+			MDC.put("statusCode", String.valueOf(response.getStatus()));
+			log.info("HTTP Request info");
+			MDC.clear();
+		}
+	}
+}
