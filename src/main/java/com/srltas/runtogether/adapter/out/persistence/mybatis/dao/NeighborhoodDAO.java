c779d0bc77bab910f3f9072a35c02525d290c953
Srltas <kdm97912@gmail.com>
2024-11-20 23:57:32 +0900

    refactor: 모든 파라미터 생성자 추가

--- src/main/java/com/srltas/runtogether/adapter/out/persistence/mybatis/dao/NeighborhoodDAO.java
+++ src/main/java/com/srltas/runtogether/adapter/out/persistence/mybatis/dao/NeighborhoodDAO.java
@@ -1,5 +1,6 @@
 package com.srltas.runtogether.adapter.out.persistence.mybatis.dao;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class NeighborhoodDAO {
 	private String id;
 	private String name;
