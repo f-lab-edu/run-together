6055457cb115f1055904bfc6c21cd2f3dc5a801d
Srltas <kdm97912@gmail.com>
2024-11-22 19:29:25 +0900

    refactor: DAO setter 삭제

--- src/main/java/com/srltas/runtogether/adapter/out/persistence/mybatis/dao/NeighborhoodDAO.java
+++ src/main/java/com/srltas/runtogether/adapter/out/persistence/mybatis/dao/NeighborhoodDAO.java
@@ -3,10 +3,8 @@ package com.srltas.runtogether.adapter.out.persistence.mybatis.dao;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class NeighborhoodDAO {
