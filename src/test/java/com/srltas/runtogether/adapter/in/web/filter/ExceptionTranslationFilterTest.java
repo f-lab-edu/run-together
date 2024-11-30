ef3b2a155542ff7cf7411cbce0066c48feb0dcb9
Srltas <kdm97912@gmail.com>
2024-11-30 23:13:07 +0900

    test: ExceptionTranslationFilter 테스트 코드 추가

---
+++ src/test/java/com/srltas/runtogether/adapter/in/web/filter/ExceptionTranslationFilterTest.java
@@ -0,0 +1,68 @@
+package com.srltas.runtogether.adapter.in.web.filter;
+
+import static com.srltas.runtogether.common.exception.code.CommonErrorCode.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.srltas.runtogether.adapter.in.exception.UnauthorizedException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+class ExceptionTranslationFilterTest {
+
+	@Test
+	@DisplayName("예외가 발생하지 않을 때 필터 처리가 없는지 확인 테스트")
+	void testDoFilter_NoException_PassesThrough() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		ExceptionTranslationFilter filter = new ExceptionTranslationFilter();
+		filter.doFilter(request, response, chain);
+
+		verify(chain, times(1)).doFilter(request, response);
+	}
+
+	@Test
+	@DisplayName("UnauthorizedException 처리 테스트")
+	void testDoFilter_UnauthorizedException_HandlesException() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		doThrow(new UnauthorizedException(UNAUTHORIZED_REQUEST)).when(chain).doFilter(request, response);
+
+		ExceptionTranslationFilter filter = new ExceptionTranslationFilter();
+		filter.doFilter(request, response, chain);
+
+		assertThat(response.getStatus(), is(HttpStatus.UNAUTHORIZED.value()));
+		assertThat(response.getContentAsString(), is(containsString(UNAUTHORIZED_REQUEST.getMessage())));
+	}
+
+	@Test
+	@DisplayName("다른 예외가 발생했을 때 재발생하는지 테스트")
+	void testDoFilter_OtherException_RethrowsException() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		FilterChain chain = mock(FilterChain.class);
+
+		doThrow(new RuntimeException("Runtime_Exception")).when(chain).doFilter(request, response);
+
+		ExceptionTranslationFilter filter = new ExceptionTranslationFilter();
+		RuntimeException exception = assertThrows(RuntimeException.class,
+			() -> filter.doFilter(request, response, chain));
+
+		assertThat(exception.getMessage(), is("Runtime_Exception"));
+	}
+}
