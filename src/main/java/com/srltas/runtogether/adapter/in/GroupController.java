08e3df962b7f890059b9b84c70a7bd8607986758
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2025-01-15 01:07:11 +0900

    [#100] 러닝 그룹 삭제 Controller 계층 구현 (#101)

--- src/main/java/com/srltas/runtogether/adapter/in/GroupController.java
+++ src/main/java/com/srltas/runtogether/adapter/in/GroupController.java
@@ -4,15 +4,19 @@ import static com.srltas.runtogether.adapter.in.web.common.SessionUtils.*;
 import static com.srltas.runtogether.adapter.in.web.common.UrlConstants.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.srltas.runtogether.adapter.in.web.dto.AddGroupRequest;
+import com.srltas.runtogether.adapter.in.web.dto.DeleteGroupRequest;
 import com.srltas.runtogether.adapter.out.session.UserSession;
 import com.srltas.runtogether.application.port.in.AddGroup;
 import com.srltas.runtogether.application.port.in.AddGroupCommand;
+import com.srltas.runtogether.application.port.in.DeleteGroup;
+import com.srltas.runtogether.application.port.in.DeleteGroupCommand;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,12 +30,13 @@ import lombok.RequiredArgsConstructor;
 public class GroupController {
 
 	private final AddGroup addGroup;
+	private final DeleteGroup deleteGroup;
 
 	@Operation(
-		summary = "그룹 생성",
-		description = "내 동네에 그룹을 생성합니다."
+		summary = "러닝 그룹 생성",
+		description = "내 동네에 러닝 그룹을 생성합니다."
 	)
-	@ApiResponse(responseCode = "200", description = "그룹 생성 성공")
+	@ApiResponse(responseCode = "200", description = "러닝 그룹 생성 성공")
 	@PostMapping
 	public ResponseEntity<Void> create(
 		@RequestBody @Valid AddGroupRequest request, HttpSession session) {
@@ -41,6 +46,17 @@ public class GroupController {
 			request.groupDescription(),
 			request.neighborhoodId(),
 			userSession.userId()));
+		return ResponseEntity.ok().build();
+	}
+
+	@Operation(
+		summary = "러닝 그룹 삭제",
+		description = "내 동네에 생성한 러닝 그룹을 삭제합니다."
+	)
+	@ApiResponse(responseCode = "200", description = "러닝 그룹 삭제 성공")
+	@DeleteMapping
+	public ResponseEntity<Void> delete(@RequestBody @Valid DeleteGroupRequest request) {
+		deleteGroup.delete(new DeleteGroupCommand(request.groupId()));
 		return ResponseEntity.ok().build();
 	}
 }
